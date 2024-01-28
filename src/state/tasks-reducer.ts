24e94eeb5e9130dfaae9c3fb2969d9450c5836d3
dnisaev <dnisaev@yandex.ru>
2024-01-28 22:28:16 +0300

    update state in reducers

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -37,16 +37,16 @@ export type ChangeTaskTitleActionType = {
 }
 
 const initialState = {
-    [todolistID1]: [
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: false},
-
-    ],
-    [todolistID2]: [
-        {id: v1(), title: 'Rest API', isDone: true},
-        {id: v1(), title: 'GraphQL', isDone: false},
-    ]
+    // [todolistID1]: [
+    //     {id: v1(), title: 'HTML&CSS', isDone: true},
+    //     {id: v1(), title: 'JS', isDone: true},
+    //     {id: v1(), title: 'ReactJS', isDone: false},
+    //
+    // ],
+    // [todolistID2]: [
+    //     {id: v1(), title: 'Rest API', isDone: true},
+    //     {id: v1(), title: 'GraphQL', isDone: false},
+    // ]
 };
 
 export const tasksReducer = (state: TasksStateType = initialState, action: ActionsType): TasksStateType => {
--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -34,8 +34,8 @@ export let todolistID1 = v1();
 export let todolistID2 = v1();
 
 const initialState: Array<TodolistsType> = [
-    {id: todolistID1, title: 'What to learn', filter: 'all'},
-    {id: todolistID2, title: 'What to buy', filter: 'all'}
+    // {id: todolistID1, title: 'What to learn', filter: 'all'},
+    // {id: todolistID2, title: 'What to buy', filter: 'all'}
 ];
 
 export const todolistsReducer = (state: Array<TodolistsType> = initialState, action: ActionsType): Array<TodolistsType> => {
