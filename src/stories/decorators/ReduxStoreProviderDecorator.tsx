2a5d1828346c6c27b82cb4d0c4cc21d56c9a922b
dnisaev <dnisaev@yandex.ru>
2024-02-19 19:00:32 +0300

    fix initialGlobalState

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -5,6 +5,9 @@ import {tasksReducer} from "../../features/TodolistsList/tasks-reducer";
 import {todolistsReducer} from "../../features/TodolistsList/todolists-reducer";
 import {thunk} from "redux-thunk";
 import {appReducer} from "../../app/app-reducer";
+import {AppRootStateType} from "../../app/store";
+import {v1} from "uuid";
+import {TaskPriorities, TaskStatuses} from "../../api/todolists-api";
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -12,22 +15,26 @@ const rootReducer = combineReducers({
     app: appReducer
 })
 
-// const initialGlobalState: AppRootStateType = {
-//     todolists: [
-//         {id: "todolistId1", title: "What to learn", filter: "all", addedDate: '', order: 0},
-//         {id: "todolistId2", title: "What to buy", filter: "all", addedDate: '', order: 0}
-//     ] ,
-//     tasks: {
-//         ["todolistId1"]: [
-//             {id: v1(), title: "HTML&CSS", status: TaskStatuses.Completed, todoListId: "todolistId1", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
-//             {id: v1(), title: "JS",status: TaskStatuses.New, todoListId: "todolistId1", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
-//         ],
-//         ["todolistId2"]: [
-//             {id: v1(), title: "Milk", status: TaskStatuses.New, todoListId: "todolistId2", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
-//             {id: v1(), title: "React Book", status: TaskStatuses.Completed, todoListId: "todolistId2", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
-//         ]
-//     }
-// };
+const initialGlobalState: AppRootStateType = {
+    todolists: [
+        {id: "todolistId1", title: "What to learn", filter: "all", entityStatus: "idle", addedDate: '', order: 0},
+        {id: "todolistId2", title: "What to buy", filter: "all", entityStatus: "idle",addedDate: '', order: 0}
+    ] ,
+    tasks: {
+        ["todolistId1"]: [
+            {id: v1(), title: "HTML&CSS", status: TaskStatuses.Completed, todoListId: "todolistId1", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: v1(), title: "JS",status: TaskStatuses.New, todoListId: "todolistId1", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
+        ],
+        ["todolistId2"]: [
+            {id: v1(), title: "Milk", status: TaskStatuses.New, todoListId: "todolistId2", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: v1(), title: "React Book", status: TaskStatuses.Completed, todoListId: "todolistId2", description: '', startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
+        ]
+    },
+    app: {
+        status: 'idle',
+        error: null
+    }
+};
 
 // @ts-ignore
 export const storyBookStore = legacy_createStore(rootReducer, applyMiddleware(thunk));
